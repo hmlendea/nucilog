fe603401946415908e4ae1cd85cf9306e29b74fc
Horatiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-05-09 14:35:38 +0300

    LogInfo constructor overloads

--- NuciLog.Core/LogInfo.cs
+++ NuciLog.Core/LogInfo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace NuciLog.Core
 {
     public sealed class LogInfo
@@ -10,6 +12,18 @@ namespace NuciLog.Core
         {
             Key = key;
             Value = value;
+        }
+
+        public LogInfo(LogInfoKey key, DateTime value, string format)
+        {
+            Key = key;
+            Value = value.ToString(format);
+        }
+
+        public LogInfo(LogInfoKey key, object value)
+        {
+            Key = key;
+            Value = value.ToString();
         }
     }
 }
