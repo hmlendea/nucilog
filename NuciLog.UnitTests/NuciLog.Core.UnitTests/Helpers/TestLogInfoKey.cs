bd5942a06f3a0cfb575b21df3b490a4eccf0c29a
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-07-23 17:15:58 +0300

    More Logger overloads

--- NuciLog.UnitTests/NuciLog.Core.UnitTests/Helpers/TestLogInfoKey.cs
+++ NuciLog.UnitTests/NuciLog.Core.UnitTests/Helpers/TestLogInfoKey.cs
@@ -11,5 +11,7 @@ namespace NuciLog.Core.UnitTests.Helpers
         }
 
         public static LogInfoKey TestKey => new TestLogInfoKey(nameof(TestKey));
+
+        public static LogInfoKey TestKey2 => new TestLogInfoKey(nameof(TestKey2));
     }
 }
