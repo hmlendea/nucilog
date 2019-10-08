e24bfe2a90496d37976ff327513108a50586a1fe
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-10-08 15:09:44 +0300

    IEquatable implementation for LogInfoKey

--- NuciLog.Core/LogInfoKey.cs
+++ NuciLog.Core/LogInfoKey.cs
@@ -1,13 +1,31 @@
-﻿namespace NuciLog.Core
+﻿using System;
+
+namespace NuciLog.Core
 {
-    public class LogInfoKey
+    public class LogInfoKey : IEquatable<LogInfoKey>
     {
+        public string Name { get; protected set; }
+
         protected LogInfoKey(string name)
         {
             Name = name;
         }
 
-        public string Name { get; protected set; }
+        public bool Equals(LogInfoKey other)
+            => Name == other.Name;
+        
+        public override bool Equals(object other)
+        {
+            if (other is LogInfoKey)
+            {
+                return Equals(other as LogInfoKey);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+            => Name.GetHashCode();
 
         internal static LogInfoKey SourceContext => new LogInfoKey(nameof(SourceContext));
 
